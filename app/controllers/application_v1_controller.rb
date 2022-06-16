d766854cb124fdd630a882318855af29670e01bc
Renato Coelho <renatocoelho.artdesign@gmail.com>
2022-06-16 08:25:34 +0100

    Refactor of characters and groups controllers to be inside modules Api and V1. Refactor of character and group routes to include CRUD actions.

--- app/controllers/application_v1_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-    module V1
-        class ApplicationV1Controller < ApplicationController
-        end
-    end
-end
-
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :characters, only: %i[index]
-      resources :groups, only: []
+      resources :characters, only: %i[index show create update destroy]
+      resources :groups, only: %i[index show create update destroy]
     end
   end
 
