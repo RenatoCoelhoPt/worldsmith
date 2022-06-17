c3c2348381e3a082327dba877dfa466fc4464969
Renato Coelho <renatocoelho.artdesign@gmail.com>
2022-06-17 14:52:38 +0100

    Refactor deactivation of Rails CSRF through ApplicationController to allow API requests from Postman.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    # TODO: Remove the line bellow and configure the Rails CSRF to accept requests from the frontend
+    protect_from_forgery with: :null_session
 end
