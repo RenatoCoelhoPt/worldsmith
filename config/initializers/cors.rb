1d6677441a9c9b8cbdc86670baddf153ea9a2903
Renato Coelho <renatocoelho.artdesign@gmail.com>
2022-06-26 22:57:19 +0100

    Fix cors.rb file, typo was impeding all rails commands.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    # TODO: Replace * in origin with 'localhost:3001'
     allow do
-        origins '*' # TODO: Replace with 'localhost:3001'
-        resources '*', headers: :any, methods: %i[get post patch put delete]
+        origins '*' 
+        resource '*', headers: :any, methods: %i[get post patch put delete]
     end
 end  
